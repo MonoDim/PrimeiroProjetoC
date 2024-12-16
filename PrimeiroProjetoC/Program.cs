155488de36d2869c236207343ded204c8239a154
Gabriel Ramos <sanchesramosg@gmail.com>
2024-12-16 17:22:26 -0300

    Programa 0.1
    nada funciona ainda (AINDA)

--- PrimeiroProjetoC/Program.cs
+++ PrimeiroProjetoC/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ouvindo Musga
+string mensagemDeBoasVindas = "Seja bem vindo ao Ouvindo Musga";
+Console.WriteLine(mensagemDeBoasVindas);

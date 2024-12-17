e9dae6a25c3fdbe7be9f727ac6166713a0affb74
Gabriel Ramos <sanchesramosg@gmail.com>
2024-12-17 11:11:04 -0300

    Projeto 0.3
    Agora tem uma arte legal (To com fome depois termino).

--- PrimeiroProjetoC/Program.cs
+++ PrimeiroProjetoC/Program.cs
@@ -1,3 +1,63 @@
 ﻿// Ouvindo Musga
-string mensagemDeBoasVindas = "Seja bem vindo ao Ouvindo Musga";
-Console.WriteLine(mensagemDeBoasVindas);
+string mensagemDeBoasVindas = (@"
+ ▄██████▄  ███    █▄   ▄█    █▄   ▄█     ▄████████                
+███    ███ ███    ███ ███    ███ ███    ███    ███                
+███    ███ ███    ███ ███    ███ ███▌   ███    ███                
+███    ███ ███    ███ ███    ███ ███▌  ▄███▄▄▄▄██▀                
+███    ███ ███    ███ ███    ███ ███▌ ▀▀███▀▀▀▀▀                  
+███    ███ ███    ███ ███    ███ ███  ▀███████████                
+███    ███ ███    ███ ███    ███ ███    ███    ███                
+ ▀██████▀  ████████▀   ▀██████▀  █▀     ███    ███                
+                                        ███    ███                
+   ▄▄▄▄███▄▄▄▄   ███    █▄     ▄████████    ▄██████▄     ▄████████
+ ▄██▀▀▀███▀▀▀██▄ ███    ███   ███    ███   ███    ███   ███    ███
+ ███   ███   ███ ███    ███   ███    █▀    ███    █▀    ███    ███
+ ███   ███   ███ ███    ███   ███         ▄███          ███    ███
+ ███   ███   ███ ███    ███ ▀███████████ ▀▀███ ████▄  ▀███████████
+ ███   ███   ███ ███    ███          ███   ███    ███   ███    ███
+ ███   ███   ███ ███    ███    ▄█    ███   ███    ███   ███    ███
+  ▀█   ███   █▀  ████████▀   ▄████████▀    ████████▀    ███    █▀ 
+");
+
+void ExecutarMensagemInicial()
+{
+    Console.WriteLine(mensagemDeBoasVindas);
+};
+
+void ExibirItensMenu()
+{
+    Console.WriteLine("Digite 1 para registara uma banda.");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas.");
+    Console.WriteLine("Digite 3 para avaliar uma banda.");
+    Console.WriteLine("Digite 4 para exibir a media de uma banda.");
+    Console.WriteLine("Digite 5 para sair.");
+    Console.Write("\nDigite aqui sua opção: ");
+
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: MensagemOpcaoEscolhida(opcaoEscolhidaNumerica);
+            break;
+        case 2: MensagemOpcaoEscolhida(opcaoEscolhidaNumerica);
+            break;
+        case 3: MensagemOpcaoEscolhida (opcaoEscolhidaNumerica);
+            break;
+        case 4: MensagemOpcaoEscolhida(opcaoEscolhidaNumerica);
+            break;
+        case 5: Console.WriteLine("Adeus!");
+            break;
+        default: Console.WriteLine("Mensagem invalida!");
+            break;
+    }
+};
+
+void MensagemOpcaoEscolhida(int opcaoEscolhidaNumerica)
+{
+    Console.WriteLine($"Você escolheu a opção: {opcaoEscolhidaNumerica}");
+};
+
+
+ExecutarMensagemInicial();
+ExibirItensMenu();

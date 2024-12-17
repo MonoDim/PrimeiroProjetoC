2e245df86a91b6d92bfa7aa49e0c810989211182
Gabriel Ramos <sanchesramosg@gmail.com>
2024-12-17 17:03:23 -0300

    Protejo 0.4
    Ele ta funcionando mais do que devia agora.
    Amanha vai fazer mais ainda

--- PrimeiroProjetoC/Program.cs
+++ PrimeiroProjetoC/Program.cs
@@ -18,6 +18,7 @@ string mensagemDeBoasVindas = (@"
  ███   ███   ███ ███    ███    ▄█    ███   ███    ███   ███    ███
   ▀█   ███   █▀  ████████▀   ▄████████▀    ████████▀    ███    █▀ 
 ");
+List<string> listaDasBandas = new List<string>();
 
 void ExecutarMensagemInicial()
 {
@@ -26,6 +27,7 @@ void ExecutarMensagemInicial()
 
 void ExibirItensMenu()
 {
+    ExecutarMensagemInicial();
     Console.WriteLine("Digite 1 para registara uma banda.");
     Console.WriteLine("Digite 2 para mostrar todas as bandas.");
     Console.WriteLine("Digite 3 para avaliar uma banda.");
@@ -38,9 +40,9 @@ void ExibirItensMenu()
     
     switch (opcaoEscolhidaNumerica)
     {
-        case 1: MensagemOpcaoEscolhida(opcaoEscolhidaNumerica);
+        case 1: RegistrarBanda();
             break;
-        case 2: MensagemOpcaoEscolhida(opcaoEscolhidaNumerica);
+        case 2: MostrarBandas();
             break;
         case 3: MensagemOpcaoEscolhida (opcaoEscolhidaNumerica);
             break;
@@ -58,6 +60,38 @@ void MensagemOpcaoEscolhida(int opcaoEscolhidaNumerica)
     Console.WriteLine($"Você escolheu a opção: {opcaoEscolhidaNumerica}");
 };
 
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("====================");
+    Console.WriteLine("=Registro de bandas=");
+    Console.WriteLine("====================\n");
+    Console.Write("Digite o nome da banda que deseja registrar:");
+    string nomeDaBanda = Console.ReadLine()!;
+    listaDasBandas.Add(nomeDaBanda);
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirItensMenu();
+}
 
-ExecutarMensagemInicial();
+void MostrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("====================");
+    Console.WriteLine("=Bandas Registradas=");
+    Console.WriteLine("====================\n");
+    for (int i = 0; i < listaDasBandas.Count; i++)
+    {
+        int o = i;
+        Console.WriteLine($"{o+1}ªBanda: {listaDasBandas[i]}");
+
+    };
+    Console.WriteLine("\nAperte qualquer tecla para sair");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirItensMenu();
+}
+
+//Execuções
 ExibirItensMenu();

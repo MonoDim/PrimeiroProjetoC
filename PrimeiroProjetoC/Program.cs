6f2e5fc427a0af801639bf492a4b2bb9a6896bab
Gabriel Ramos <sanchesramosg@gmail.com>
2024-12-18 15:55:23 -0300

    Projeto 0.6
    Chega de programa por hj (Tenho q jogar lol)

--- PrimeiroProjetoC/Program.cs
+++ PrimeiroProjetoC/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Ouvindo Musga
+using Microsoft.Win32;
+
 string mensagemDeBoasVindas = (@"
  ▄██████▄  ███    █▄   ▄█    █▄   ▄█     ▄████████                
 ███    ███ ███    ███ ███    ███ ███    ███    ███                
@@ -18,7 +20,11 @@ string mensagemDeBoasVindas = (@"
  ███   ███   ███ ███    ███    ▄█    ███   ███    ███   ███    ███
   ▀█   ███   █▀  ████████▀   ▄████████▀    ████████▀    ███    █▀ 
 ");
-List<string> listaDasBandas = new List<string>();
+//List<string> listaDasBandas = new List<string>();
+
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Link no Parquinho", new List<int> { 5, 5, 9 });
+bandasRegistradas.Add("Os Bezouros", new List<int>());
 
 void ExecutarMensagemInicial()
 {
@@ -27,6 +33,7 @@ void ExecutarMensagemInicial()
 
 void ExibirItensMenu()
 {
+    Console.Clear();
     ExecutarMensagemInicial();
     Console.WriteLine("Digite 1 para registara uma banda.");
     Console.WriteLine("Digite 2 para mostrar todas as bandas.");
@@ -44,7 +51,7 @@ void ExibirItensMenu()
             break;
         case 2: MostrarBandas();
             break;
-        case 3: MensagemOpcaoEscolhida (opcaoEscolhidaNumerica);
+        case 3: AvaliarBandasRegistradas();
             break;
         case 4: MensagemOpcaoEscolhida(opcaoEscolhidaNumerica);
             break;
@@ -63,12 +70,10 @@ void MensagemOpcaoEscolhida(int opcaoEscolhidaNumerica)
 void RegistrarBanda()
 {
     Console.Clear();
-    Console.WriteLine("====================");
-    Console.WriteLine("=Registro de bandas=");
-    Console.WriteLine("====================\n");
+    ExibirTituloPagina("Registro de bandas");
     Console.Write("Digite o nome da banda que deseja registrar:");
     string nomeDaBanda = Console.ReadLine()!;
-    listaDasBandas.Add(nomeDaBanda);
+    bandasRegistradas.Add(nomeDaBanda, new List<int>());
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
     Console.ReadKey();
     Console.Clear();
@@ -78,16 +83,14 @@ void RegistrarBanda()
 void MostrarBandas()
 {
     Console.Clear();
-    Console.WriteLine("====================");
-    Console.WriteLine("=Bandas Registradas=");
-    Console.WriteLine("====================\n");
+    ExibirTituloPagina("Bandas Registradas");
     //for (int i = 0; i < listaDasBandas.Count; i++)
     //{
         //int o = i;
         //Console.WriteLine($"{o+1}ªBanda: {listaDasBandas[i]}");
 
     //};
-    foreach (string banda in listaDasBandas)
+    foreach (string banda in bandasRegistradas.Keys)
     {
         Console.WriteLine($"Banda: {banda}");
     }
@@ -95,6 +98,34 @@ void MostrarBandas()
     Console.ReadKey();
     Console.Clear();
     ExibirItensMenu();
+}
+
+void ExibirTituloPagina(string titulo)
+{
+    int quantidadeDeLetras = (titulo.Length) + 2;
+    string charactereIgual = string.Empty.PadLeft(quantidadeDeLetras, '=');
+    Console.WriteLine(charactereIgual);
+    Console.WriteLine($"={titulo}=");
+    Console.WriteLine(charactereIgual + "\n");
+}
+
+void AvaliarBandasRegistradas()
+{
+    Console.Clear();
+    ExibirTituloPagina("Avaliador de bandas");
+    Console.Write("Digite aqui a banda que você gostaria de avaliar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        
+    }
+    else
+    {
+        Console.WriteLine("\nEssa banda ainda não está registrada. ;-;");
+        Console.WriteLine("Aperta um botão para voltar para o menu legal.");
+        Console.ReadKey();
+        ExibirItensMenu();
+    }
 }
 
 //Execuções
